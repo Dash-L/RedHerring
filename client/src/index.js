2b2bb8c0caa1260f8488dbdc6295fd7e4f1810d2
Dashiell Elliott <dashiell.elliott@yahoo.com>
2020-11-22 12:58:41 -0500

    remove old client

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-//Renders app at root div
-ReactDOM.render(<App />, document.getElementById('root'));
